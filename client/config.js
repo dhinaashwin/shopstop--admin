de389eef621b87a06b749caaac42494969147d83
dhinaashwin <dhinaashwin11@gmail.com>
2024-06-30 15:30:15 +0530

    c-4

--- client/config.js
+++ client/config.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTH,
-  projectId:process.env.PID ,
-  storageBucket:process.env.SBUCKET ,
-  messagingSenderId:process.env.SID ,
-  appId: process.env.APPID,
-  measurementId: process.env.MID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTH,
+  projectId:process.env.REACT_APP_PID ,
+  storageBucket:process.env.REACT_APP_SBUCKET ,
+  messagingSenderId:process.env.REACT_APP_SID ,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
