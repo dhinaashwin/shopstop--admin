468695cacbb3c407aa2c2ce1ef2a73111eb20534
dhinaashwin <dhinaashwin11@gmail.com>
2024-06-30 15:14:00 +0530

    c-3

--- client/config.js
+++ client/config.js
@@ -5,19 +5,18 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+require('dotenv').config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDV8A0dgMFeSJ8WumSGI_YBXULp92kQKAI",
-  authDomain: "fir-c4fa9.firebaseapp.com",
-  projectId: "fir-c4fa9",
-  storageBucket: "fir-c4fa9.appspot.com",
-  messagingSenderId: "63836388369",
-  appId: "1:63836388369:web:47886a975ae3e80b3405b0",
-  measurementId: "G-JQBZSJ32ST"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTH,
+  projectId:process.env.PID ,
+  storageBucket:process.env.SBUCKET ,
+  messagingSenderId:process.env.SID ,
+  appId: process.env.APPID,
+  measurementId: process.env.MID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
