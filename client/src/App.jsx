358ead254f7d9962a60c9de1b4714699a9dd0e8a
dhinaashwin <dhinaashwin11@gmail.com>
2024-07-01 08:28:56 +0530

    update App.jsx and main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -175,7 +175,7 @@ function App() {
       <div>
         <h1>Add Products</h1>
         <div>
-          <label htmlFor="name">Id</label>
+          <label htmlFor="id">Id</label>
           <input
             type="number"
             value={id}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Upload from './Upload.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Upload />
+    <App />
   </React.StrictMode>,
 )
