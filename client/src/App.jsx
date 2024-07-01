a33c14227461c3844b99ec843fabe646cd27da88
dhinaashwin <dhinaashwin11@gmail.com>
2024-07-01 07:50:43 +0530

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,102 +1,68 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { imageDb } from "../config"; // Ensure this file exports the configured Firebase storage instance
 import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid"; // Import the v4 function and alias it as uuidv4 for generating unique IDs
 import "./App.css";
-import { useAppReducer } from "./store"; // Import the custom hook from store.js
 
 function App() {
-  const [state, dispatch] = useAppReducer();
-
-  const fileInputRef = useRef(); // Create a ref for the file input
-  const fileInputRef2 = useRef(); // Create a ref for the second file input
-  const fileInputRef3 = useRef();
-  const fileInputRef4 = useRef();
+ 
+  const [uploadStatus, setUploadStatus] = useState("");
+  const [id, setId] = useState(null);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [newProduct, setNewProduct] = useState(false);
+  const [discount, setDiscount] = useState(false);
+  const [gender, setGender] = useState("");
+  const [showAllItems, setShowAllItems] = useState(false); // State to toggle showing all items
+  const [dresses, setDresses] = useState([]);
+  const [sizes, setSizes] = useState({
+    S: 0,
+    M: 0,
+    L: 0,
+    XL: 0,
+    XXL: 0,
+    XXXL:0,
+  });
 
   const handleClick = async () => {
-    if (!state.img) {
-      dispatch({ type: "SET_UPLOAD_STATUS", uploadStatus: "No main image selected" });
+    if (!img) {
+      setUploadStatus("No main image selected");
       return;
     }
-    if (!state.name || !state.price || !state.category || !state.gender) {
+    if (!name || !price || !category || !gender) {
       alert("Please fill out all required fields.");
       return;
     }
     try {
-      const uploadImage = async (file) => {
-        const imgRef = ref(imageDb, `files/${file.name}-${uuidv4()}`);
-        await uploadBytes(imgRef, file);
-        return getDownloadURL(imgRef);
-      };
-
-      const mainImageUrl = await uploadImage(state.img);
-      const image2Url = state.img2 ? await uploadImage(state.img2) : "";
-      const image3Url = state.img3 ? await uploadImage(state.img3) : "";
-      const image4Url = state.img4 ? await uploadImage(state.img4) : "";
-
-      const postData = {
-        id: state.id,
-        name: state.name,
-        price: parseFloat(state.price),
-        imageUrl: mainImageUrl,
-        image_2: image2Url,
-        image_3: image3Url,
-        image_4: image4Url,
-        category: state.category,
-        discount: state.discount,
-        gender: state.gender,
-        new_product: state.newProduct,
-        sizes: state.sizes,
-      };
-
-      // Log the data being posted
-      console.log("Posting data:", postData);
-
       // Send data to MongoDB via your backend server
       await fetch("https://shopstop-admin-server.vercel.app/upload", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(postData),
+        body: JSON.stringify({
+          id,
+          name,
+          price: parseFloat(price),
+          category: category,
+          discount: discount,
+          gender: gender,
+          new_product: newProduct,
+          sizes,
+        }),
       });
 
-      dispatch({ type: "SET_UPLOAD_STATUS", uploadStatus: "Upload and data save successful" });
-      fetchImages();
+      setUploadStatus("Upload and data save successful");
       fetchDresses();
-      alert("Form uploaded successfully!"); // Add alert message here
       resetForm(); // Reset the form after upload
     } catch (error) {
-      dispatch({ type: "SET_UPLOAD_STATUS", uploadStatus: `Upload failed: ${error.message}` });
-    }
-  };
-
-  const handleImageChange = (e, imageField, previewField) => {
-    const file = e.target.files[0];
-    dispatch({
-      type: "SET_IMAGE",
-      imageField,
-      previewField,
-      image: file,
-      preview: URL.createObjectURL(file),
-    });
-  };
-
-  const fetchImages = async () => {
-    try {
-      const listRef = ref(imageDb, "files");
-      const res = await listAll(listRef);
-      const urls = await Promise.all(
-        res.items.map((item) => getDownloadURL(item))
-      );
-      dispatch({ type: "SET_IMAGES_URLS", imgUrls: urls });
-    } catch (error) {
-      console.error("Failed to fetch images:", error);
+      setUploadStatus(`Upload failed: ${error.message}`);
     }
   };
 
   const toggleShowItems = () => {
-    dispatch({ type: "TOGGLE_SHOW_ITEMS" });
+    setShowAllItems((prev) => !prev); // Toggle showAllItems state
   };
 
   const fetchDresses = async () => {
@@ -105,7 +71,7 @@ function App() {
         "https://shopstop-admin-server.vercel.app/items"
       );
       const data = await response.json();
-      dispatch({ type: "SET_DRESSES", dresses: data });
+      setDresses(data);
     } catch (error) {
       console.error("Failed to fetch dresses:", error);
     }
@@ -117,7 +83,7 @@ function App() {
         await fetch(`https://shopstop-admin-server.vercel.app/items/${id}`, {
           method: "DELETE",
         });
-        dispatch({ type: "SET_DRESSES", dresses: state.dresses.filter((dress) => dress._id !== id) });
+        setDresses(dresses.filter((dress) => dress._id !== id));
       } catch (error) {
         console.error("Failed to delete dress:", error);
       }
@@ -125,15 +91,25 @@ function App() {
   };
 
   const resetForm = () => {
-    dispatch({ type: "RESET_FORM" });
-    fileInputRef.current.value = null; // Clear the file input
-    fileInputRef2.current.value = null; // Clear the second file input
-    fileInputRef3.current.value = null;
-    fileInputRef4.current.value = null;
+  
+    setId(null);
+    setSizes({
+      S: 0,
+      M: 0,
+      L: 0,
+      XL: 0,
+      XXL: 0,
+      XXXL:0,
+    });
+    setName("");
+    setPrice("");
+    setDiscount("");
+    setCategory("");
+    setGender("");
+    setNewProduct("");
   };
 
   useEffect(() => {
-    fetchImages();
     fetchDresses();
   }, []);
 
@@ -146,177 +122,115 @@ function App() {
           <label htmlFor="id">Id</label>
           <input
             type="number"
-            value={state.id}
-            onChange={(e) => dispatch({ type: "SET_FIELD", field: "id", value: e.target.value })}
+            value={id}
+            onChange={(e) => setId(e.target.value)}
           />
           <label htmlFor="name">Name</label>
           <input
             type="text"
-            value={state.name}
-            onChange={(e) => dispatch({ type: "SET_FIELD", field: "name", value: e.target.value })}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <label htmlFor="price">Price</label>
           <input
             type="number"
-            value={state.price}
-            onChange={(e) => dispatch({ type: "SET_FIELD", field: "price", value: e.target.value })}
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
           />
           <label htmlFor="category">Category</label>
           <input
             type="text"
-            value={state.category}
-            onChange={(e) => dispatch({ type: "SET_FIELD", field: "category", value: e.target.value })}
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
           />
           <label>
             New Product:
             <input
               type="checkbox"
-              checked={state.newProduct}
-              onChange={(e) => dispatch({ type: "SET_FIELD", field: "newProduct", value: e.target.checked })}
+              checked={newProduct}
+              onChange={(e) => setNewProduct(e.target.checked)}
             />
           </label>
           <label>
             Discount:
             <input
               type="checkbox"
-              checked={state.discount}
-              onChange={(e) => dispatch({ type: "SET_FIELD", field: "discount", value: e.target.checked })}
+              checked={discount}
+              onChange={(e) => setDiscount(e.target.checked)}
             />
           </label>
           <label htmlFor="gender">Gender</label>
-          <select value={state.gender} onChange={(e) => dispatch({ type: "SET_FIELD", field: "gender", value: e.target.value })}>
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
             <option value="">Select</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
           <label htmlFor="sizes">Sizes:</label>
-          {Object.keys(state.sizes).map((size) => (
-            <div key={size} style={{ marginBottom: "10px" }}>
-              <div>{size}:</div>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <button
-                  onClick={() =>
-                    dispatch({
-                      type: "UPDATE_SIZES",
-                      size,
-                      value: state.sizes[size] + 1,
-                    })
-                  }
-                >
-                  +
-                </button>
-                <input
-                  type="number"
-                  id={`size-${size}`}
-                  value={state.sizes[size]}
-                  onChange={(e) =>
-                    dispatch({
-                      type: "UPDATE_SIZES",
-                      size,
-                      value: e.target.value === "" ? "" : parseInt(e.target.value),
-                    })
-                  }
-                  style={{ width: "50px", textAlign: "center", margin: "0 10px" }}
-                />
-                <button
-                  onClick={() =>
-                    dispatch({
-                      type: "UPDATE_SIZES",
-                      size,
-                      value: Math.max(state.sizes[size] - 1, 0),
-                    })
-                  }
-                >
-                  -
-                </button>
-              </div>
-            </div>
-          ))}
+{Object.keys(sizes).map((size) => (
+  <div key={size} style={{ marginBottom: '10px' }}>
+    <label htmlFor={`size-${size}`}>{size}</label>
+    <div>
+      <button
+        onClick={() =>
+          setSizes((prevSizes) => ({
+            ...prevSizes,
+            [size]: {
+              ...prevSizes[size],
+              value: (prevSizes[size]?.value || 0) + 1,
+            },
+          }))
+        }
+      >
+        +
+      </button>
+      <input
+        type="number"
+        id={`size-${size}`}
+        value={sizes[size]?.value || 0}
+        onChange={(e) =>
+          setSizes((prevSizes) => ({
+            ...prevSizes,
+            [size]: {
+              ...prevSizes[size],
+              value: e.target.value === '' ? '' : parseInt(e.target.value),
+            },
+          }))
+        }
+        style={{ width: '50px', textAlign: 'center', margin: '0 10px' }}
+      />
+      <button
+        onClick={() =>
+          setSizes((prevSizes) => ({
+            ...prevSizes,
+            [size]: {
+              ...prevSizes[size],
+              value: Math.max((prevSizes[size]?.value || 0) - 1, 0),
+            },
+          }))
+        }
+      >
+        -
+      </button>
+    </div>
+  </div>
+))}
         </div>
-        <input
-          type="file"
-          ref={fileInputRef}
-          onChange={(e) => handleImageChange(e, "img", "previewUrl")}
-        />
-        {state.previewUrl && (
-          <div>
-            <img
-              src={state.previewUrl}
-              alt="Selected"
-              style={{ maxWidth: "200px", marginTop: "10px" }}
-            />
-          </div>
-        )}
-        <input
-          type="file"
-          ref={fileInputRef2}
-          onChange={(e) => handleImageChange(e, "img2", "previewUrl2")}
-        />
-        {state.previewUrl2 && (
-          <div>
-            <img
-              src={state.previewUrl2}
-              alt="Selected"
-              style={{ maxWidth: "200px", marginTop: "10px" }}
-            />
-          </div>
-        )}
-        <input
-          type="file"
-          ref={fileInputRef3}
-          onChange={(e) => handleImageChange(e, "img3", "previewUrl3")}
-        />
-        {state.previewUrl3 && (
-          <div>
-            <img
-              src={state.previewUrl3}
-              alt="Selected"
-              style={{ maxWidth: "200px", marginTop: "10px" }}
-            />
-          </div>
-        )}
-        <input
-          type="file"
-          ref={fileInputRef4}
-          onChange={(e) => handleImageChange(e, "img4", "previewUrl4")}
-        />
-        {state.previewUrl4 && (
-          <div>
-            <img
-              src={state.previewUrl4}
-              alt="Selected"
-              style={{ maxWidth: "200px", marginTop: "10px" }}
-            />
-          </div>
-        )}
         <button onClick={handleClick}>Upload</button>
-        {state.uploadStatus && <p>{state.uploadStatus}</p>}
+        {uploadStatus && <p>{uploadStatus}</p>}
       </div>
       <div>
         {/* right side */}
         <button onClick={toggleShowItems}>
-          {state.showAllItems ? "Hide All Items" : "Show All Items"}
+          {showAllItems ? "Hide All Items" : "Show All Items"}
         </button>
-        {state.showAllItems && (
+        {showAllItems && (
           <div>
             <h2>Dresses:</h2>
             <ul>
-              {state.dresses.map((dress) => (
+              {dresses.map((dress) => (
                 <li key={dress._id}>
-                  <img
-                    src={dress.imageUrl}
-                    alt={dress.name}
-                    style={{ maxWidth: "200px" }}
-                  />
                   <p>Name: {dress.name}</p>
                   <p>Price: {dress.price}</p>
-                  {dress.image_2 && (
-                    <img
-                      src={dress.image_2}
-                      alt={`${dress.name} - 2`}
-                      style={{ maxWidth: "200px" }}
-                    />
-                  )}
                   <button onClick={() => handleDelete(dress._id)}>
                     Delete Item
                   </button>
